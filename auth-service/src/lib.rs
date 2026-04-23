cc52bcacd7ed0396083d0a2cd18d8a5c68c8b198
da-moon <github.com.3qrsg@passmail.net>
2026-04-23 18:07:03 -0400

    Sprint 1 - Task 5 (#2)
    * test: add helpers
    
    Signed-off-by: da-moon <contact@havi.dev>
    
    * feat: missing code for sprint 1 task 3
    
    Signed-off-by: da-moon <contact@havi.dev>
    
    * test: fixed mocks
    
    Signed-off-by: da-moon <contact@havi.dev>
    
    ---------
    
    Signed-off-by: da-moon <contact@havi.dev>

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,58 @@
+use std::error::Error;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::post, serve::Serve, Router};
+use tokio::net::TcpListener;
+use tower_http::services::{ServeDir, ServeFile};
+
+pub struct Application {
+    server: Serve<TcpListener, Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+// login
+// logout
+// verify-2fa
+// verify-token
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let asset_dir =
+            ServeDir::new("assets").not_found_service(ServeFile::new("assets/index.html"));
+        let router = Router::new()
+            .fallback_service(asset_dir)
+            // TODO: Add all other routes
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/logout", post(logout))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/verify-token", post(verify_token));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
