4f56c8d7f0b87d81692b693b9c5923e0ff17c0f6
da-moon <contact@havi.dev>
2026-04-23 21:52:10 +0000

    feat: missing code for sprint 1 task 3
    Signed-off-by: da-moon <contact@havi.dev>

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+
+use axum::{routing::post, serve::Serve, Router};
+use tokio::net::TcpListener;
+use tower_http::services::{ServeDir, ServeFile};
+
+pub struct Application {
+    server: Serve<TcpListener, Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Move the Router definition from `main.rs` to here.
+        // Also, remove the `hello` route.
+        // We don't need it at this point!
+        let router = Router::new();
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
